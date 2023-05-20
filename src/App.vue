4474ed45fea05ec3e42178f71f5b5088eedd77b8
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-05-20 19:06:46 -0400

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<!--  <header>-->
-<!--    <div class="wrapper">-->
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--        <RouterLink to="/pullrequests">PullRequests</RouterLink>-->
-<!--      </nav>-->
-<!--    </div>-->
-<!--  </header>-->
-
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     <RouterLink to="/dashboard">Dashboard</RouterLink>
-<!--    <TheWelcome />-->
   </main>
 </template>
