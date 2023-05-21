42ce642509f8a5c979f60902c996fddec053807f
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-05-20 20:22:48 -0400

    Home page style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
-  <main>
-    <RouterLink to="/dashboard">Dashboard</RouterLink>
-  </main>
+  <div class="hero is-fullheight">
+    <div class="container is-flex is-align-items-center is-justify-content-center">
+      <h1 class="title is-3 has-text-centered">
+        <RouterLink to="/dashboard">GitHub PullRequests<br>Dashboard</RouterLink>
+      </h1>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+
+</style>
